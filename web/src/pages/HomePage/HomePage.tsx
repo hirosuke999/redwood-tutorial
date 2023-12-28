43b4638524fba53c396f888217da19bcb3b38b06
ysenga <ysenga@example.com>
2023-12-28 13:29:33 +0900

    feat: add views for home and about

---
+++ web/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <Metadata title="Home" description="Home page" />
+
+      <main>Home</main>
+    </>
+  )
+}
+
+export default HomePage
