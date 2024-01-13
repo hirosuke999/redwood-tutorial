e609c9a82c48d02aa8b7cfd46806ee32d7a1fbe6
ysenga <ysenga@example.com>
2024-01-13 15:06:09 +0900

    test: update article test suite

--- web/src/components/ArticlesCell/ArticlesCell.test.tsx
+++ web/src/components/ArticlesCell/ArticlesCell.test.tsx
@@ -1,4 +1,5 @@
-import { render } from '@redwoodjs/testing/web'
+import { render, screen, within } from '@redwoodjs/testing/web'
+
 import { Loading, Empty, Failure, Success } from './ArticlesCell'
 import { standard } from './ArticlesCell.mock'
 
@@ -27,15 +28,19 @@ describe('ArticlesCell', () => {
     }).not.toThrow()
   })
 
-  // When you're ready to test the actual output of your component render
-  // you could test that, for example, certain text is present:
-  //
-  // 1. import { screen } from '@redwoodjs/testing/web'
-  // 2. Add test: expect(screen.getByText('Hello, world')).toBeInTheDocument()
-
   it('renders Success successfully', async () => {
-    expect(() => {
-      render(<Success articles={standard().articles} />)
-    }).not.toThrow()
+    const articles = standard().articles
+    render(<Success articles={articles} />)
+
+    articles.forEach((article) => {
+      const truncateBody = article.body.substring(0, 10)
+      const matchedBody = screen.getByText(truncateBody, { exact: false })
+      const ellipsis = within(matchedBody).getByText('...', { exact: false })
+
+      expect(screen.getByText(article.title)).toBeInTheDocument()
+      expect(screen.queryByText(article.body)).not.toBeInTheDocument()
+      expect(matchedBody).toBeInTheDocument()
+      expect(ellipsis).toBeInTheDocument()
+    })
   })
 })
